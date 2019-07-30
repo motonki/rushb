48cc2e7b94906c5dde6d955e9eed3cd2f747e692
Juho Pekkinen <juho.pekkinen@iki.fi>
2019-07-30 08:46:40 +0300

    Initialise the project

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
 function App() {
@@ -9,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Hello world!
         </p>
         <a
           className="App-link"
